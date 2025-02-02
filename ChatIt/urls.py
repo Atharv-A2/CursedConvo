8d2921bcfb93fd2f23ea4a79e029cdedadea5935
Atharv Agrawal <atharv1901@gmail.com>
2025-02-02 18:11:09 +0530

    Enhanced the UI
    Changed the Templates and removed the username from the URL and made it to Popup

--- ChatIt/urls.py
+++ ChatIt/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat/<str:username>/', views.chat_room, name='chat_room'),
+    path('chat/', views.chat_room, name='chat_room'),
     path('online/', views.online_users_view, name='online_users'),
 ]
--- ChatIt/views.py
+++ ChatIt/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-def chat_room(request, username):
-    return render(request, "chat/chat_room.html", {"username" : username})
+def chat_room(request):
+    return render(request, "chat/chat_room.html")
 
 def online_users_view(request):
     return render(request, "chat/online_users.html")
